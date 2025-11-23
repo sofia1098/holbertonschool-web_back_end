b23263ff2d372bb191ee5edf93f1e1988d5c052b
sofia1098 <sofianunez000@gmail.com>
2025-11-23 21:38:15 +0000

    0

---
+++ 0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def add (a: float, b: float) -> float:
+    """
+    Suma dos números de tipo float y retorna su resultado.
+    
+    Args:
+        a: Primer número flotante
+        b: Segundo número flotante
+    
+    Returns:
+        La suma de a y b como float
+    """
+    return a + b
