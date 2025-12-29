468dc565f9d15af8d7340c56f041fb510acd6d24
sofia1098 <sofianunez000@gmail.com>
2025-12-29 15:02:51 +0000

    Add: docstring to index_range function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-""" Simple function for pagination """
+""" Simple function for pagination. """
 
 
 def index_range(page: int, page_size: int) -> tuple:
+    """Funcion que retorna una tupla con el comienzo y final."""
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
