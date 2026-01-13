053b6227eb8951b73b8dc905580c6302dfc742b2
sofia1098 <sofianunez000@gmail.com>
2026-01-13 20:04:07 +0000

    Add README and task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudent() {
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Fransico',
+    },
+    {
+      id: 2,
+      fisrtName: 'James',
+      location: 'Columbia',
+    },
+    {
+        id: 2,
+        fisrtName: 'James',
+        location: 'Columbia',
+      },
+    ];
+}
