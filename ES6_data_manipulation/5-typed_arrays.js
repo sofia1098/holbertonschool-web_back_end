5724461cc319172bb689f3dbc21aede92d467be2
sofia1098 <sofianunez000@gmail.com>
2026-01-14 14:49:36 +0000

    Add task 5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,14 @@
+export default createInt8TypedArray(length,position,value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+    ArrayBuffer += (value)
+    console.log(00)
+
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  
+    view.setInt8(position, value);
+  
+    return view;
+}
