597ea9135e637fa67fcdebfb286c54ec45b58f00
sofia1098 <sofianunez000@gmail.com>
2026-01-19 14:12:06 +0000

    Add task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
