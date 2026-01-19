004ba1f33427884caba2585e22dade58ed8e988f
sofia1098 <sofianunez000@gmail.com>
2026-01-19 14:18:19 +0000

    Fix task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,18 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+function boldIs(text) {
+  return text.replace(/is/g, '\x1b[1mis\x1b[0m');
+}
+
+process.stdout.write(
+  boldIs('Welcome to Holberton School, what is your name?\n')
+  );
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  console.log(boldIs(`Your name is: ${name}`));
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  console.log(
+    boldIs('This important software is now closing')
+  );
 });
