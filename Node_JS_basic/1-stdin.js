d048ff057faa7c34e4351063b3fbb46730b5b9f1
sofia1098 <sofianunez000@gmail.com>
2026-01-19 15:10:50 +0000

    4

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,2 @@
-function boldIs(text) {
-  return text.replace(/is/g, '\x1b[1mis\x1b[0m');
-}
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdout.write(
-  boldIs('Welcome to Holberton School, what is your name?\n')
-  );
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(boldIs(`Your name is: ${name}`));
-});
-
-process.stdin.on('end', () => {
-  console.log(
-    boldIs('This important software is now closing')
-  );
-});
