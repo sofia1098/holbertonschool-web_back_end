cbdc409b0d6b093f506ffa9e8c984f298eb6bed0
sofia1098 <sofianunez000@gmail.com>
2026-01-19 15:07:43 +0000

    Fix task 5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,4 @@
 export default function createInt8TypedArray(length, position, value) {
-  // Check if the position is within the bounds of the array length
   if (position >= length || position < 0) {
     throw new Error('Position outside range');
   }
